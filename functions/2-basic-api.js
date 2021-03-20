279df2d23973f636ac39ff8b596596efb3c9500a
Vania Dimova <ivankadimov8@gmail.com>
2021-03-20 18:49:11 -0400

    core error fixed

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,6 +2,9 @@ const items = require ('../assets/data')
 
 exports.handler = async (event, context)  => {
     return {
+        headers: {
+            'Access-Control-Origin' : '*',
+        },
         statusCode: 200,
         body: JSON.stringify(items),
     }
