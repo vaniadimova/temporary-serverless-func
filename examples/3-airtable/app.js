a90b9b4b086c3dda025c3d3ef771f8f96abc16c1
Vania Dimova <ivankadimov8@gmail.com>
2021-03-22 13:11:39 -0400

    function

--- examples/3-airtable/app.js
+++ examples/3-airtable/app.js
@@ -1,1 +1,27 @@
 const result = document.querySelector('.result')
+
+const fetchProducts = async () =>  {
+    try {
+        //const {data} = await axios.get('/api/3-airtable') 
+       // const id = window.location.search
+    
+        const {data} = await axios.get('/api/3-z-complete') 
+        const products = data.map((product) => {
+            const {id, url, name, price} = product
+            return `<a href="product.html?id=${id}'' class="product">
+            <img src="${url}" alt="${name}"/>
+            <div class="info">
+            <h5>${name}</h5>
+            <h5 class="price">$${price}</h5>
+
+            </div>
+            </a>`
+        }).join('')
+        result.innerHTML = products
+    } catch (error) {
+        result.innerHTML = '<h4>ERROR</h4>'
+    }
+  
+} 
+
+fetchProducts()
---
+++ functions/3-product.js
@@ -0,0 +1,35 @@
+require('dotenv').config()
+const Airtable = require('airtable-node');
+ 
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY})
+  .base('appUDon2hD21hcIdJ')
+  .table('products')
+
+exports.handler = async (event, context) => {
+    const { id } = event.queryStringParameters 
+
+    if (id) {
+        try {
+          const product = await airtable.retrieve(id)
+          if (product.error) {
+            return {
+              statusCode: 404,
+              body: `No product with id: ${id}`,
+            }
+          }
+          return {
+            statusCode: 200,
+            body: JSON.stringify(product),
+          }
+        } catch (error) {
+          return {
+            statusCode: 500,
+            body: `Server Error`,
+          }
+        }
+      }
+      return {
+        statusCode: 400,
+        body: 'Please provide product id',
+      }
+    }
